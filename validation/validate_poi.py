784ddb5ea761affe959851d5aa8ff83d0836c2de
Sateesh Kadiyala <skadiyala@qci.com>
2017-05-25 07:22:09 -0500

    eigenfaces, validate_poi

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,8 +12,11 @@
 
 import pickle
 import sys
+from sklearn.tree import DecisionTreeClassifier
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import model_selection
+from sklearn.metrics import accuracy_score
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -25,8 +28,20 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+#print "labels", labels
+#print "features", features
+
+clf = DecisionTreeClassifier()
 
 
-### it's all yours from here forward!  
+features_train, features_test, labels_train, labels_test = model_selection.train_test_split(features,
+                                                                                            labels,
+                                                                                             test_size=0.3,
+                                                                                             random_state=42)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test, labels_test)
+
+print "accuracy score", accuracy_score(labels_test, pred)
+
 
 
